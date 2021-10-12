cebe73c64886cbfde6e7f34319d7a5dc896329b0
Aula L 5-2 <l52@academico.unab.ad>
2021-10-12 15:36:16 -0500

    cambiado documento ProductoDao, agregado insert, update, delete, y busqueda por id

--- app/src/main/java/com/example/tiendaapp/ProductoDao.java
+++ app/src/main/java/com/example/tiendaapp/ProductoDao.java
@@ -1,7 +1,10 @@
 package com.example.tiendaapp;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Update;
 
 import java.util.List;
 
@@ -11,5 +14,15 @@ public interface ProductoDao {
     @Query("select * from productos")
     List<Producto> obtenerProducto();
 
-    
+    @Insert
+    void agregar(Producto producto);
+
+    @Update
+    void editar(Producto producto);
+
+    @Delete
+    void eliminar(Producto producto);
+
+    @Query("select * from productos where id = :parametro")
+    Producto obtenerPorId(int parametro);
 }
